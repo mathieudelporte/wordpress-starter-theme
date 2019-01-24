6a196dabe79a236c3cb3e03f0863e570f64b49d3
Mathieu Delporte - Belintra <Mathieu.Delporte@belintra.com>
2019-01-24 08:40:50 +0100

    index.php loop

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
-<?php get_header(); the_post(); ?>
+<?php get_header(); ?>
 
 	<!-- Content -->
 	<main class="full-width white pad-full">
 		<div class="wrapper">
-			<?php the_content(); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<article>
+					<?php the_content(); ?>
+				</article>
+			<?php endwhile; else : ?>
+				<p>Geen resultaten</p>
+			<?php endif; ?>
 		</div>
 	</main>
 	<!-- /Content -->
