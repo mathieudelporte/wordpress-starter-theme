de8a8425ac2b6905361dbd9c5d1a9b12996fcb22
Mathieu Delporte - Belintra <Mathieu.Delporte@belintra.com>
2019-02-12 09:54:42 +0100

    Gulp basic setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+concat = require( 'gulp-concat' );
 
-exports.default = defaultTask
+gulp.task( 'sass', function() {
+  return gulp.src('src/scss/main.scss')
+    .pipe(sass( {outputStyle: 'compressed'} ))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest(''))
+} );
+
+gulp.task( 'watch', function() {
+	gulp.watch( 'src/scss/**/*.scss', ['sass'] );
+} );
+
+gulp.task( 'default', [ 'sass' ], function() {
+	console.log( 'default gulp task...' )
+} );
